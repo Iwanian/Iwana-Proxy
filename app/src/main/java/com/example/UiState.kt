ff11db14445113b776d5db3371969c4a02c0feab
Iwana? <iwanabedi@gmail.com>
2026-07-02 22:22:53 +0330

    Delete app directory

--- app/src/main/java/com/example/UiState.kt
+++
@@ -1,7 +0,0 @@
-package com.example
-
-sealed interface UiState {
-    object Loading : UiState
-    data class Success(val proxies: List<ProxyItem>) : UiState
-    data class Error(val message: String) : UiState
-}
